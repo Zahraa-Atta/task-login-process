3a4cc132d3a73e0519fbba332d90cc7bee8a3535
Zahraa Atta <zahraaatta5@gmail.com>
2024-11-08 05:01:05 +1100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import MainWelcome from './components/Welcome/Welcome'
-import MainSeleact from './Screen/Seleact/seleact'
-
+import MainWelcome from './components/Welcome/welcome'
 
 
 createRoot(document.getElementById('root')).render(
