25ac8178ceeedc2b7e861c89e858521a54cae47d
Zahraa Atta <zahraaatta5@gmail.com>
2024-11-08 22:32:17 +0300

    add more files

--- src/Screen/Otp/otp.jsx
+++ src/Screen/Otp/otp.jsx
@@ -1,25 +1,58 @@
 import React, { useState } from 'react';
-import { OtpInput } from 'reactjs-otp-input'; 
-import './otp'
+import { OtpInput } from 'reactjs-otp-input';
+import './otp.css';
+
 const Otp = () => {
-  const [otp, setOtp] = useState('');  
+  const [otp, setOtp] = useState('');
+  const [isValid, setIsValid] = useState(false);
+
   const handleOtpChange = (otp) => {
     setOtp(otp);
+    setIsValid(otp.length === 6);
+  };
 
-    setIsButtonDisabled(otp.length !== 6); 
+  const btnpath = () => {
+    if (isValid) {
+      console.log("تم إرسال الرمز");
+    }
   };
 
   return (
-    <div className="otpstyle"> 
-    <OtpInput 
-      value={otp} 
-      onChange={handleOtpChange} 
-      numInputs={6}  
-      separator={<span></span>}  
-      containerStyle="otp-container-style"
-      inputStyle="otp-input-style"
-    />
-
+    <div className="otpstyle">
+      <div className="top-bar">
+        <div className="line-blue"></div>
+        <div className="line-blue"></div>
+        <div className="line-gray"></div>
+      </div>
+      <div className="info">
+        <h2>Enter Your Phone Number</h2>
+        <div className="subInfo">
+          <p>
+            Enter your phone number to verify your identity and the validity of
+            your number to safely activate your account on the platform.
+          </p>
+        </div>
+      </div>
+      <OtpInput 
+        value={otp} 
+        onChange={handleOtpChange} 
+        numInputs={6}  
+        className="otp-container-style"  // استخدم className بدلاً من containerStyle
+      />
+      <div className="btn">
+        <button 
+          onClick={btnpath} 
+          style={{
+            backgroundColor: isValid ? 'rgba(60, 151, 175, 1)' : '', 
+            cursor: isValid ? 'pointer' : 'not-allowed',  
+            color: isValid ? 'rgba(255, 255, 255, 1)' : '',
+            fontWeight: isValid ? 'bold' : 'normal'
+          }}
+          disabled={!isValid}
+        >
+          Send verification code
+        </button>
+      </div>
     </div>
   );
 };
